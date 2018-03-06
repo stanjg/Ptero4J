ec10d0c5e0a85633fcccf84c9482803828fd7393
stanjg <stan@gabes.nl>
2018-03-06 14:00:13 +0100

    Added methods to fetch all users and from a specific page

--- src/main/java/com/stanjg/ptero4j/PteroAPI.java
+++ src/main/java/com/stanjg/ptero4j/PteroAPI.java
@@ -8,9 +8,12 @@ import com.stanjg.ptero4j.util.PteroUtils;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PteroAPI {
 
@@ -28,7 +31,63 @@ public class PteroAPI {
             e.printStackTrace();
         }
 
-        PteroUtils.log(getUser(1).getEmail());
+        PteroUtils.log(getUsers(4534).size());
+    }
+
+    public List<User> getUsers(int page) {
+
+        try {
+            Response response = makeApiCall("/users?page="+page, HTTPMethod.GET);
+            if (response.code() != 200) {
+                return null;
+            }
+
+            List<User> users = new ArrayList<>();
+
+            JSONObject json = new JSONObject(response.body().string());
+            JSONArray arr = json.getJSONArray("data");
+            for (int i = 0; i < arr.length(); i++) {
+                users.add(new User(arr.getJSONObject(i).getJSONObject("attributes")));
+            }
+
+            return users;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public List<User> getUsers() {
+
+        try {
+            Response response = makeApiCall("/users", HTTPMethod.GET);
+            if (response.code() != 200) {
+                return null;
+            }
+
+            List<User> users = new ArrayList<>();
+
+            JSONObject json = new JSONObject(response.body().string());
+            int pages = json.getJSONObject("meta").getJSONObject("pagination").getInt("total_pages");
+            for (int i = 1; i <= pages; i++) {
+                if (i != 1)
+                    json = new JSONObject(makeApiCall("/users?page="+i, HTTPMethod.GET).body().string());
+
+                JSONArray arr = json.getJSONArray("data");
+                for (int j = 0; j < arr.length(); j ++) {
+                    JSONObject juser = arr.getJSONObject(j);
+
+                    users.add(new User(juser.getJSONObject("attributes")));
+                }
+            }
+
+            return users;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public User getUser(int id) {
