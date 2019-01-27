4aff7ca4d9edbffacfd6ffd0f9e354d37e083917
ErdbeerbaerLP <27149563+ErdbeerbaerLP@users.noreply.github.com>
2019-01-27 15:21:55 +0100

    Add documentation to ServerUsage

--- src/main/java/com/stanjg/ptero4j/entities/objects/server/ServerUsage.java
+++ src/main/java/com/stanjg/ptero4j/entities/objects/server/ServerUsage.java
@@ -7,12 +7,21 @@ public class ServerUsage {
 		this.cpu = cpu;
 		this.memory = memory;
 	}
+	/**
+	* @return CPU usage in percent
+	*/
 	public int getCpuUsage() {
 		return cpu;
 	}
+	/**
+	* @return Memory usage in MB
+	*/
 	public int getMemoryUsage() {
 		return memory;
 	}
+	/**
+	* @return Disk usage in MB
+	*/
 	public int getDiskUsage() {
 		return disk;
 	}
